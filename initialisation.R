e00c008a8dd79e60bdeaff7e84711b060368c182
Abhilash Lakshman <46469834+abhilashlakshman@users.noreply.github.com>
2020-04-03 11:36:50 -0400

    Add files via upload

---
+++ initialisation.R
@@ -0,0 +1,15 @@
+is.installed <- function(mypkg) is.element(mypkg, installed.packages()[,1])
+
+
+if (!require("plotly")) install.packages("plotly",dependencies = TRUE)
+if (!require("shiny")) install.packages("shiny",dependencies = TRUE)
+if (!require("shinydashboard")) install.packages("shinydashboard",dependencies = TRUE)
+if (!require("pracma")) install.packages("pracma",dependencies = TRUE)
+if (!require("circular")) install.packages("circular",dependencies = TRUE)
+
+
+if (is.installed("plotly") & is.installed("shiny") & is.installed("shinydashboard") & is.installed("zoo") & is.installed("pracma") & is.installed("RColorBrewer") & is.installed("zeitgebr")){
+  print(paste('Initialisation complete! The program is now ready for use'), quote=F)
+} else {
+  print(paste('Error: One or more packages may not have installed correctly'), quote=F)
+}
